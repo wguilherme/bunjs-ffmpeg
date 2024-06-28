5c4b923a8941fd32ae856aa64b80bc850712c27e
Withney Guilherme <wguilhermecriativo@gmail.com>
2024-06-27 21:50:13 -0300

    feat: add generate video with images and then add audio

--- index.ts
+++ index.ts
@@ -3,17 +3,18 @@ import path from 'path';
 
 const baseVideo = path.join(__dirname, '/assets/video1.mp4');
 const image = path.join(__dirname, '/assets/cupom.jpeg');
-const outputVideo = path.join(__dirname, '/assets/output.mp4');
 const sound = path.join(__dirname, '/assets/sound.mp3');
 
-// Unify the video and the image
+const outputVideo = path.join(__dirname, '/assets/output.mp4');
+const outputVideoWithSound = path.join(__dirname, '/assets/output2.mp4');
 
 function createVideo() {
-  ffmpeg(baseVideo)
+  ffmpeg()
+    .input(baseVideo)
     .input(image)
     .input(image)
     .input(sound)
-    .inputOptions(['-t 2'])
+    .inputOptions(['-t 4']) // Definindo a duração total do vídeo para 4 segundos
     .complexFilter([
       {
         filter: 'overlay',
@@ -22,8 +23,8 @@ function createVideo() {
           y: '(main_h-overlay_h)/2',
           enable: 'between(t,1,2)',
         },
-        inputs: ['0:v', '1:v'], // 0:v é o vídeo base, 1:v é a primeira imagem
-        outputs: 'overlay1'
+        inputs: ['0:v', '1:v'], // 0:v é o vídeo base, 1:v é a imagem para overlay
+        outputs: 'output1'
       },
       {
         filter: 'overlay',
@@ -32,13 +33,13 @@ function createVideo() {
           y: '(main_h-overlay_h)/2',
           enable: 'between(t,3,4)',
         },
-        inputs: ['overlay1', '2:v'], // overlay1 é o output do primeiro overlay, 2:v é a segunda imagem
-        outputs: 'final'
-      }
-    ], 'final')
+        inputs: ['output1', '1:v'], // output1 é o resultado do primeiro overlay, 1:v é a segunda imagem
+        outputs: 'output2'
+      },
+    ], 'output2')
     .output(outputVideo)
-    .on('start', () => {
-      console.log('Creating video...');
+    .on('start', (command) => {
+      console.log('Creating video...', command);
     })
     .on('end', () => {
       console.log('Video created');
@@ -49,4 +50,32 @@ function createVideo() {
     .run();
 }
 
+function addSoundToVideo(){
+  ffmpeg()
+  .input(outputVideo)
+  .input(sound)
+  .outputOptions(['-c:v copy', '-c:a aac', '-strict experimental'])
+  .output(outputVideoWithSound)
+  .on('start', (command) => {
+    console.log('Adding sound to video...', command);
+  })
+  .on('end', () => {
+    console.log('Sound added to video');
+  })
+  .on('error', (err) => {
+    console.error('Error: ' + err.message);
+  })
+  .run();
+}
+
 createVideo();
+
+// Aguardar a criação do vídeo para adicionar o som
+setTimeout(() => {
+  addSoundToVideo();
+}, 3000);
+
+
+
+
+
